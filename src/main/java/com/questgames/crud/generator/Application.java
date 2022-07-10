76678ef596843866d6f3c91b3e9cc0814e1a1859
Gabriel Giuseppe do Nascimento <gabriel.giuseppe@live.com>
2022-07-10 14:39:18 -0300

    Initial Conf

---
+++ src/main/java/com/questgames/crud/generator/Application.java
@@ -0,0 +1,13 @@
+package com.questgames.crud.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
